7469ae71265b8646acdb3992cfbec935a2b07be5
Kyungmin Kim <kyungddin@cau.ac.kr>
2025-07-29 01:09:05 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 76 ms, Memory: 12492 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> visit(100001, -1);
+vector<int> map[100001];
+
+int cnt = 0;
+
+void DFS(int num, int depth);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m, r, i, j;
+
+	cin >> n >> m >> r;
+
+	int a, b;
+	for (i = 0; i < m; i++) {
+		cin >> a >> b;
+		map[a].push_back(b);
+		map[b].push_back(a);
+	}
+
+	for (i = 1; i <= n; i++) {
+		sort(map[i].begin(), map[i].end(), greater<int>());
+	}
+
+	DFS(r, 0);
+
+	for (i = 1; i <= n; i++) {
+		cout << visit[i] << "\n";
+	}
+}
+
+void DFS(int num, int depth) {
+	visit[num] = depth;
+
+	int i;
+	for (i = 0; i < map[num].size(); i++) {
+		if (visit[map[num][i]] == -1) {
+			DFS(map[num][i], depth+1);
+		}
+	}
+}
