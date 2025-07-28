08fa73e70032ab788c86fa1b8cf4632bed1b19ec
Kyungmin Kim <kyungddin@cau.ac.kr>
2025-07-29 08:31:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 4, Time: 72 ms, Memory: 9232 KB -BaekjoonHub

---
+++ 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> visit(100001, 0);
+vector<int> depth(100001, -1);
+vector<int> map[100001];
+queue<int> q;
+
+int cnt = 0;
+
+void BFS(int num);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m, r, i;
+
+	cin >> n >> m >> r;
+
+	int a, b;
+	for (i = 0; i < m; i++) {
+		cin >> a >> b;
+		map[a].push_back(b);
+		map[b].push_back(a);
+	}
+
+	for (i = 1; i <= n; i++) {
+		sort(map[i].begin(), map[i].end());
+	}
+
+	BFS(r);
+
+	long long sum = 0;
+
+	for (i = 1; i <= n; i++) {
+		sum += 1LL * depth[i] * visit[i];
+	}
+
+	cout << sum;
+}
+
+void BFS(int num) {
+	int tmp, i;
+
+	cnt++;
+	visit[num] = cnt;
+	depth[num] = 0;
+	q.push(num);
+
+	while (q.size() != 0) {
+		tmp = q.front();
+		q.pop();
+
+		for (i = 0; i < map[tmp].size(); i++) {
+			if (visit[map[tmp][i]] == 0) {
+				cnt++;
+				depth[map[tmp][i]] = depth[tmp] + 1;
+				visit[map[tmp][i]] = cnt;
+				q.push(map[tmp][i]);
+			}
+		}
+	}
+}
