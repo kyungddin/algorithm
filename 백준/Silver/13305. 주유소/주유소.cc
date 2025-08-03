82870fd3982bd268e85bda1cb2505b7827c1a7b4
Kyungmin Kim <kyungddin@cau.ac.kr>
2025-08-03 17:41:59 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 3588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,52 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-vector<pair<int, int>> city;
-vector<int> weight;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    int n;
+    cin >> n;
 
-	int i;
+    vector<long long> dist(n - 1); 
+    vector<long long> cost(n);
 
-	int n;
-	cin >> n;
+    for (int i = 0; i < n - 1; ++i) {
+        cin >> dist[i];
+    }
 
-	int tmp;
-	for (i = 0; i < n - 1; i++) {
-		cin >> tmp;
-		weight.push_back(tmp);
-	}
+    for (int i = 0; i < n; ++i) {
+        cin >> cost[i];
+    }
 
-	for (i = 0; i < n-1; i++) {
-		cin >> tmp;
-		city.push_back(pair<int, int>(tmp, weight[i]));
-	}
+    long long total_cost = 0;
+    long long min_cost = cost[0];
 
-	cin >> tmp;
-	city.push_back(pair<int, int>(tmp, 0));
+    for (int i = 0; i < n - 1; ++i) {
+        if (cost[i] < min_cost) {
+            min_cost = cost[i]; 
+        }
+        total_cost += min_cost * dist[i];
+    }
 
-	int price = 0;
-	int distance = 0;
-	int first_city = 0;
-
-	for (i = 1; i < n; i++) {
-		if (city[first_city].first >= city[i].first) {
-			distance += city[i-1].second;
-			price += (city[first_city].first * distance);
-			distance = 0;
-			first_city = i;
-		}
-		else {
-			distance += city[i - 1].second;
-		}
-	}
-
-	cout << price;
+    cout << total_cost;
 }
