98948032c48c1d4a2a591c6d28d1409cc4001f42
Kyungmin Kim <kyungddin@cau.ac.kr>
2025-08-04 20:18:27 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> map[9];
+vector<bool> visit(9, 0);
+vector<int> tmp;
+
+void DFS(int num, int depth, int limit) {
+	visit[num] = true;
+	tmp.push_back(num);
+
+	if (depth == limit) {
+		for (int j = 0; j < tmp.size(); j++) {
+			cout << tmp[j] << " ";
+		}
+		cout << "\n";
+
+		visit[num] = false;
+		tmp.pop_back();
+		return;
+	}
+
+	for (int i = 0; i < map[num].size(); i++) {
+		int next = map[num][i];
+		if (!visit[next]) {
+			DFS(next, depth + 1, limit);
+		}
+	}
+
+	visit[num] = false;
+	tmp.pop_back();
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j <= n; j++) {
+			if (i != j) map[i].push_back(j);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		fill(visit.begin(), visit.end(), 0);
+		DFS(i, 1, m);
+	}
+}
