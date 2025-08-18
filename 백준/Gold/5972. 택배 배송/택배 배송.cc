b1e196a7106eb21778d5f05f98d154f1571e13ee
Kyungmin Kim <kyungddin@cau.ac.kr>
2025-08-18 12:54:52 +0900

    [Gold V] Title: 택배 배송, Time: 28 ms, Memory: 5536 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<pair<int, int>> graph[50001];
+vector<int> dist(50001);
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int V, E;
+    cin >> V >> E;
+
+    dist.resize(V + 1);
+    fill(dist.begin(), dist.end(), 999999999);
+
+    dist[1] = 0;
+
+    for (int i = 0; i < E; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back(make_pair(b, c));
+        graph[b].push_back(make_pair(a, c));
+    }
+
+    q.push(make_pair(0, 1));
+
+    while (!q.empty()) {
+        int curr_dist = q.top().first;
+        int u = q.top().second;
+        q.pop();
+
+        if (curr_dist > dist[u]) continue;
+
+        for (auto edge : graph[u]) {
+            int v = edge.first;
+            int w = edge.second;
+
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                q.push(make_pair(dist[v], v));
+            }
+        }
+    }
+
+    cout << dist[V];
+}
