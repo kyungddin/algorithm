bc9003d0c510965d983d19cfe6cd0385bfd0c2e3
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-04-04 16:37:11 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,62 +1,34 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
+
 using namespace std;
 
-vector<int> heap;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-void push(int num) {
-    heap.push_back(num);
-    int child = heap.size() - 1;
-    while (child > 0) {
-        int parent = (child - 1) / 2;
-        if (heap[child] < heap[parent]) {
-            swap(heap[child], heap[parent]);
-            child = parent;
-        }
-        else break;
-    }
-}
+	priority_queue<int, vector<int>, greater<int>> min_heap;
 
-int pop() {
-    if (heap.empty()) return 0;
-    int result = heap[0];
-    heap[0] = heap.back();
-    heap.pop_back();
+	int n;
+	cin >> n;
 
-    int parent = 0;
-    int size = heap.size();
-    while (true) {
-        int left = parent * 2 + 1;
-        int right = parent * 2 + 2;
-        int largest = parent;
+	int tmp;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		
+		if (tmp == 0)
+		{
+			if (min_heap.empty()) cout << "0\n";
+			else {
+				cout << min_heap.top() << "\n";
+				min_heap.pop();
+			}
+		}
+		else min_heap.push(tmp);
+	}
 
-        if (left < size && heap[left] < heap[largest]) largest = left;
-        if (right < size && heap[right] < heap[largest]) largest = right;
-
-        if (largest == parent) break;
-        swap(heap[parent], heap[largest]);
-        parent = largest;
-    }
-    return result;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n;
-    cin >> n;
-    while (n--) {
-        int num;
-        cin >> num;
-        if (num == 0) {
-            cout << pop() << "\n";
-        }
-        else {
-            push(num);
-        }
-    }
-    return 0;
+	return 0;
 }
